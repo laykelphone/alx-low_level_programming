cc9c21f7aef59058111288c10a660c1bfd7d08ae
laykelphone <laykelphone@gmail.com>
2023-10-10 14:39:34 -0700

    kl

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,14 @@
 * @n: the int to extract the last digit from
 * Return: value of the last digit
 */
-int print_last_digit(int n)
+int print_last_digit(int n
 {
-int a;        
+int a
 if (n < 0)
 n = -n;
-a=n % 10;
+a = n % 10;
 if (a < 0)
 a = -a;
-_putchar(a + '0');
+_putchar(a +'0');
 return (a);
 }
